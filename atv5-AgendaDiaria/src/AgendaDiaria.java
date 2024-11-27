be3fe9f309122cd44582134f7344057d698e98f3
yrdma <artuzoygor@gmail.com>
2024-11-26 23:56:22 -0300

    atv5-AgendaDiaria
    att

--- atv5-AgendaDiaria/src/AgendaDiaria.java
+++ atv5-AgendaDiaria/src/AgendaDiaria.java
@@ -1,13 +1,82 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+//iserir os compromissos em uma variavel e mandar para o Jtable
+//enviar data e hora com o Jspinner
+//usar o botão para excluir o compromisso
 
 public class AgendaDiaria {
 
     private JPanel mainPanel;
     private JTextField txtCompromisso;
-    private JSpinner spinDataHora;
+    private JSpinner spnHora;
     private JButton btnAddCompromisso;
     private JTable tableCompromissos;
     private JButton btnRemoverCompromisso;
+    private JSpinner spnData;
+    private JLabel lblAviso;
+
+    DefaultTableModel model = (DefaultTableModel) tableCompromissos.getModel();
+
+
+    private void configSpinners() {
+        spnData.setModel(new SpinnerDateModel());
+        JSpinner.DateEditor editorData = new JSpinner.DateEditor(spnData, "dd/MM/yyyy");
+        spnData.setEditor(editorData);
+
+
+        spnHora.setModel(new SpinnerDateModel());
+        JSpinner.DateEditor editorHora = new JSpinner.DateEditor(spnHora, "HH:mm");
+        spnHora.setEditor(editorHora);
+    }
+
+
+    public AgendaDiaria() {
+        configSpinners();
+        DefaultTableModel modeloTable = new DefaultTableModel(
+                new String[]{"Compromisso", "Data", "Hora"}, 0
+        );
+        tableCompromissos.setModel(modeloTable);
+
+
+        btnAddCompromisso.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String atividade = txtCompromisso.getText();
+                String data = new SimpleDateFormat("dd/MM/yyyy").format(spnData.getValue());
+                String hora = new SimpleDateFormat("HH:mm").format(spnHora.getValue());
+                if (atividade.isBlank()) {
+                    lblAviso.setText("Favor declare o nome da atividade");
+
+
+                } else {
+
+                    modeloTable.addRow(new Object[]{atividade, data, hora});
+
+
+                    txtCompromisso.setText("");
+                    lblAviso.setText("");
+                }
+            }
+        });
+        btnRemoverCompromisso.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int linhaSelecionada = tableCompromissos.getSelectedRow();
+
+                if(linhaSelecionada != -1) {
+                    modeloTable.removeRow(linhaSelecionada);
+                    lblAviso.setText("");
+                }else {
+                    lblAviso.setText("Selecione um compromisso para remover");
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Agenda Diária");
